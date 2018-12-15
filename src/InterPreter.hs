e5860d1fd761150070e3ce9c3d5c95f02e40c9a4
yumlonne <yumlonne@gmail.com>
2018-12-15 17:41:50 +0900

    add some functions

--- src/InterPreter.hs
+++ src/InterPreter.hs
@@ -29,3 +29,7 @@ exec :: TopOperation -> Env -> IO Env
 exec (Let var) env = return $ insert var 0 env   -- Let
 exec (Do var (Is v)) env = return $ insert var (value v env) env
 exec (Print var) env = (print (value var env)) >> return env
+
+execAll :: [TopOperation] -> Env -> IO Env
+execAll [] env = return env
+execAll list env = Prelude.foldl (\e o -> e >>= exec o) (return env) list
--- src/InterPreter/Env.hs
+++ src/InterPreter/Env.hs
@@ -2,3 +2,6 @@ module InterPreter.Env where
 import Data.Map
 
 type Env = Map String Int
+
+init :: Env
+init = fromList []
